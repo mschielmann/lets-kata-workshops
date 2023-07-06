8872bfeddafb04c53216ce7888115916582f1162
Michal Schielmann <michalschielmann@gmail.com>
2023-07-07 00:40:37 +0200

    tennis game kata done!

--- src/main/java/com/endava/letskata/workshops/gdansk/one/TennisGameApp.java
+++ src/main/java/com/endava/letskata/workshops/gdansk/one/TennisGameApp.java
@@ -1,24 +1,73 @@
 package com.endava.letskata.workshops.gdansk.one;
 
+import java.util.Map;
+
 class TennisGameApp {
+    private static final Map<Integer, String> SCORE_TO_TEXT_MAPPING = Map.of(
+            1, "Fifteen",
+            2, "Thirty",
+            3, "Forty"
+    );
 
-    private int playerOneScore = 0;
+    private final String player1;
+    private final String player2;
 
-    public TennisGameApp(final String player1, final String player2) {
-    }
+    private int player1Score = 0;
+    private int player2Score = 0;
 
-    String getScore() {
-        if (playerOneScore > 0) {
-            return "Fifteen-Love";
-        }
-        return "Love-all";
-    }
-
-    String showPlayers() {
-        return "Ann vs. Mike";
+    TennisGameApp(String player1, String player2) {
+        this.player1 = player1;
+        this.player2 = player2;
     }
 
     void scorePlayer1() {
-        playerOneScore++;
+        player1Score++;
+    }
+
+    void scorePlayer2() {
+        player2Score++;
+    }
+
+    String getScore() {
+        if (equalScores(player1Score, player2Score)) {
+            if (isOverThirty(player1Score)) {
+                return "Deuce";
+            }
+            return getPlayerScoreAsText(player1Score) + "-All";
+        }
+
+        if (anyScoreOverForty(player1Score, player2Score)) {
+            String winningPlayer = player1Score > player2Score ? player1 : player2;
+            if (isAdvantagePoint()) {
+                return "Advantage " + winningPlayer;
+            }
+            return "Game " + winningPlayer;
+        }
+
+        return getPlayerScoreAsText(player1Score) + "-" + getPlayerScoreAsText(player2Score);
+    }
+
+    private boolean equalScores(int player1Score, int player2Score) {
+        return player1Score == player2Score;
+    }
+
+    private boolean isOverThirty(int score) {
+        return score > 2;
+    }
+
+    private boolean anyScoreOverForty(int player1Score, int player2Score) {
+        return player1Score > 3 || player2Score > 3;
+    }
+
+    private boolean isAdvantagePoint() {
+        return Math.abs(player1Score - player2Score) == 1;
+    }
+
+    private String getPlayerScoreAsText(int score) {
+        return SCORE_TO_TEXT_MAPPING.getOrDefault(score, "Love");
+    }
+
+    String showPlayers() {
+        return player1 + " vs. " + player2;
     }
 }
--- src/test/java/com/endava/letskata/workshops/gdansk/one/TestTennisGameScoreAppShould.java
+++ src/test/java/com/endava/letskata/workshops/gdansk/one/TestTennisGameScoreAppShould.java
@@ -1,24 +1,12 @@
 package com.endava.letskata.workshops.gdansk.one;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class TestTennisGameScoreAppShould {
-
-
-    @Test
-    public void returnLoveAllAtBeginning() {
-        // GIVEN
-        TennisGameApp game = new TennisGameApp("Ann", "Mike");
-
-        // WHEN
-        String result = game.getScore();
-
-        // THEN
-        assertThat(result).isEqualTo("Love-all");
-    }
 
     @Test
     public void showPlayerNames() {
@@ -30,16 +18,45 @@ class TestTennisGameScoreAppShould {
         assertThat(result).isEqualTo("Ann vs. Mike");
     }
 
-    @Test
-    public void returnFifteenLoveWhenFirstPlayerScores() {
+    @ParameterizedTest
+    @CsvSource({
+            "0,0,Love-All",
+            "1,0,Fifteen-Love",
+            "0,1,Love-Fifteen",
+            "2,0,Thirty-Love",
+            "0,2,Love-Thirty",
+            "3,0,Forty-Love",
+            "0,3,Love-Forty",
+            "2,1,Thirty-Fifteen",
+            "1,2,Fifteen-Thirty",
+            "3,1,Forty-Fifteen",
+            "1,3,Fifteen-Forty",
+            "3,2,Forty-Thirty",
+            "2,3,Thirty-Forty",
+            "1,1,Fifteen-All",
+            "2,2,Thirty-All",
+            "3,3,Deuce",
+            "4,3,Advantage Ann",
+            "3,4,Advantage Mike",
+            "5,3,Game Ann",
+            "3,5,Game Mike",
+    })
+    public void shouldReturnProperTextRepresentationOfTheScore(int player1Score,
+                                                               int player2Score,
+                                                               String expectedResult) {
         // GIVEN
         TennisGameApp game = new TennisGameApp("Ann", "Mike");
 
         // WHEN
-        game.scorePlayer1();
+        for (int i = 0; i < player1Score; i++) {
+            game.scorePlayer1();
+        }
+        for (int i = 0; i < player2Score; i++) {
+            game.scorePlayer2();
+        }
         String result = game.getScore();
 
         // THEN
-        assertThat(result).isEqualTo("Fifteen-Love");
+        assertThat(result).isEqualTo(expectedResult);
     }
 }
